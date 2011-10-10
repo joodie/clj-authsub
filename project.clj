07542d0741c0dcf24017a3b67b0f12c47272da44
Joost Diepenmaat <joost@zeekat.nl>
2011-10-10 14:15:55 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-authsub "0.1.1"
+(defproject clj-authsub "0.1.2"
   :description "Clojure library for using google AuthSub authentication"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [clj-http "0.2.1"]])
